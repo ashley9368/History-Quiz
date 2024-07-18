134c3d60fdf56fda7fe23a7a63e9a7e7ac9aa7cf
Ashley Goward <ashleygoward@icloud.com>
2024-07-18 16:16:50 +0000

    Made start button work with js and added first question and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,35 @@
-const startBtn = document.getElementById('start-btn');
+// Variables
+const startButton = document.getElementById('start-btn'); 
 const questionContainer = document.getElementById('question-container');
+const questionElement = document.getElementById('question'); // Get the element to display answer buttons
+const answerButtonsElement = document.getElementById('answer-buttons');
 
-startBtn.addEventListener('click', beginQuiz);
+// Event listener for starting quiz
+startButton.addEventListener('click', beginQuiz);
 
+// Begin Quiz Area
 function beginQuiz() {
   console.log('Quiz started');
-  startBtn.classList.add('hide');
-  questionContainer.classList.remove('hide');
+  startButton.classList.add('hide'); // Hide the start button
+  questionContainer.classList.remove('hide'); // Unhides the question and answers
+  showQuestion(); // Displays first question and answers
 }
 
-function setNextQuestion() {
+function showQuestion() {
+  questionElement.innerText = "What year did the first world war start?";
+  answerButtonsElement.innerHTML = '';
 
-}
+  const answers = [
+    '1914',
+    '1916',
+    '1918',
+    '1920'
+  ];
 
-function selectAnswer() {
-
+  answers.forEach(answer => {
+    const button = document.createElement('button');
+    button.innerText = answer;
+    button.classList.add('btn');
+    answerButtonsElement.appendChild(button);
+  });
 }
