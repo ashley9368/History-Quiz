55f3b796b30a83f0da190eee544b2d262e929d64
Ashley Goward <ashleygoward@icloud.com>
2024-07-19 11:26:39 +0000

    added hover effect for start quiz button, and changed hover color on quiz game

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ const questionContainer = document.getElementById('question-container');
 const questionElement = document.getElementById('question'); // Get the element to display answer buttons
 const answerButtonsElement = document.getElementById('answer-buttons');
 
+// Questions for quiz
 const quizQuestions = [
   {
   question: "What year did the first World War start?",
@@ -15,6 +16,46 @@ const quizQuestions = [
   answers: ['Leonardo da Vinci', 'Pablo Picasso', 'Vincent van Gogh', 'Michelangelo'],
   correctAnswer: 0
 },
+{
+  question: "Who wrote the play 'Romeo and Juliet'?",
+  answers: ['Charles Dickens', 'William Shakespeare', 'Mark Twain', 'Jana Austen'],
+  correctAnswer: 1
+},
+{
+  question: "Who was the first person to walk on the moon?",
+  answers: ['Buzz Aldrin', 'Michael Collins', 'Yuri Gagarin', 'Neil Armstrong'],
+  correctAnswer: 3
+},
+{
+  question: "What year did the Berlin Wall fall?",
+  answers: ['1987', '1989', '1991', '1993'],
+  correctAnswer: 1
+},
+{
+  question: "Who was the British Prime Minister during World War II?",
+  answers: ['Neville Chamberlain', 'Clement Attlee', 'Winston Churchill', 'Harold Macmillan'],
+  correctAnswer: 2
+},
+{
+  question: "Which ancient civilization built the pyramids?",
+  answers: ['Mesopotamians', 'Greeks', 'Romans', 'Egyptians'],
+  correctAnswer: 3
+},
+{
+  question: "In which year did the American Civil War begin?",
+  answers: ['1859', '1860', '1861', '1862'],
+  correctAnswer: 2
+},
+{
+  question: "Which empire was ruled by Genghis Khan?",
+  answers: ['Ottoman Empire', 'Byzantine Empire', 'Mongol Empire', 'Persian Empire'],
+  correctAnswer: 2
+},
+{
+  question: "The Treaty of Versailles ended which war?",
+  answers: ['World War I', 'World War II', 'Franco-Prussian War', 'Crimean War'],
+  correctAnswer: 0
+}
 ];
 
 let currentQuestionIndex = 0;
@@ -63,6 +104,8 @@ function checkAnswer(selectedIndex) {
   }
 
   function quizComplete() {
+    // Show "Quiz Complete!" message
     questionElement.innerText = "Quiz Complete!";
+    // Remove answer buttons when quiz is complete
     answerButtonsElement.innerHTML = '';
   }
